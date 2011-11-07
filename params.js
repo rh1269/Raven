ecaee1aca853187dbdbe2495e7e417019ddcbd8a
Brendan H <cecil137@gmailc.om>
2011-11-07 16:53:23 -0600

    Experimenting with weapon seeking. Last merge seems to have stopped the weapon pickups

--- params.js
+++ params.js
@@ -36,7 +36,7 @@ var NumTeams = 2;
 //this is the maximum number of search cycles allocated to *all* current path
 // planning searches per update
 var MaxSearchCyclesPerUpdateStep = 1000;
-var StartMap = "maps/default.raven";
+var StartMap = "maps/moreWeaponsDefault.raven";
 
 //cell space partitioning defaults
 var NumCellsX = 10;
