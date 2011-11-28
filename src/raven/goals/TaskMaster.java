e47303e8b60b648f95600226e96a4d29b84a25cd
Brendan <Brendan.hinman@gmail.com>
2011-11-28 13:13:19 -0600

    Officially Added TaskMaster Class/ first TODO's

---
+++ src/raven/goals/TaskMaster.java
@@ -0,0 +1,14 @@
+package raven.goals;
+
+public class TaskMaster {
+
+	///TODO Is there a captain? Make a captain!
+	
+	///TODO Other cases
+	///If there is already a captain, let's figure out an new role
+	///wingman
+	///Medic
+	
+	
+	
+}
